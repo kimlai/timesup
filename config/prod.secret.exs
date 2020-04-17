cbddc344fb657912a14a19428bcacaf03b94ae68
kimlai <hello@kimlaitrinh.me>
2020-04-17 18:19:56 +0200

    setup for gigalixir

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -4,16 +4,9 @@
 # remember to add this file to your .gitignore.
 use Mix.Config
 
-secret_key_base =
-  System.get_env("SECRET_KEY_BASE") ||
-    raise """
-    environment variable SECRET_KEY_BASE is missing.
-    You can generate one by calling: mix phx.gen.secret
-    """
-
 config :timesup, TimesupWeb.Endpoint,
   http: [:inet6, port: String.to_integer(System.get_env("PORT") || "4000")],
-  secret_key_base: secret_key_base
+  secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
 
 # ## Using releases (Elixir v1.9+)
 #
