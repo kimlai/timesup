998f110f5056f1ee2991be5febed73b417693e57
kimlai <hello@kimlaitrinh.me>
2020-04-20 01:19:40 +0200

    Ensure :ssl is started before executing migrations
    Fixing
    
    ** (EXIT from #PID<0.165.0>) an exception was raised:
        ** (RuntimeError) SSL connection can not be established because
    `:ssl` application is not started,
    you can add it to `extra_application` in your `mix.exs`:
    
      def application do
        [extra_applications: [:ssl]]
      end
    
            (postgrex) lib/postgrex.ex:542: Postgrex.ensure_deps_started!/1
            (postgrex) lib/postgrex.ex:484: Postgrex.child_spec/1
            (ecto_sql) lib/ecto/adapters/sql.ex:459:
    Ecto.Adapters.SQL.init/3
            (ecto) lib/ecto/repo/supervisor.ex:178:
    Ecto.Repo.Supervisor.init/1
            (stdlib) supervisor.erl:295: :supervisor.init/1
            (stdlib) gen_server.erl:374: :gen_server.init_it/2
            (stdlib) gen_server.erl:342: :gen_server.init_it/6
            (stdlib) proc_lib.erl:249: :proc_lib.init_p_do_apply/3

--- lib/timesup/release.ex
+++ lib/timesup/release.ex
@@ -2,12 +2,15 @@ defmodule Timesup.Release do
   @app :timesup
 
   def migrate do
+    Application.ensure_all_started(:ssl)
+
     for repo <- repos() do
       {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :up, all: true))
     end
   end
 
   def rollback(repo, version) do
+    Application.ensure_all_started(:ssl)
     {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :down, to: version))
   end
 
