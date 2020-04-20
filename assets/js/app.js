39588956952454c6738950a2f62cf75d46bfbfa3
kimlai <hello@kimlaitrinh.me>
2020-04-20 14:22:16 +0200

    Remove phoenix_html from app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,20 +2,6 @@
 // The MiniCssExtractPlugin is used to separate it out into
 // its own CSS file.
 import css from "../css/app.css";
-
-// webpack automatically bundles all modules in your
-// entry points. Those entry points can be configured
-// in "webpack.config.js".
-//
-// Import dependencies
-//
-import "phoenix_html";
-
-// Import local files
-//
-// Local files can be imported directly using relative paths, for example:
-// import socket from "./socket"
-
 import { Socket } from "phoenix";
 import LiveSocket from "phoenix_live_view";
 import Clipboard from "clipboard";
